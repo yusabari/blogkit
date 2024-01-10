4a30b2fa3a4849f731b5a4fae788bf40bcb47245
yusabari <152364753+yusabari@users.noreply.github.com>
2024-01-10 11:46:14 +0900

    Update NotionPage.tsx

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -280,7 +280,8 @@ export const NotionPage: React.FC<types.PageProps> = ({
         mapImageUrl={mapImageUrl}
         searchNotion={config.isSearchEnabled ? searchNotion : null}
         pageAside={pageAside}
-        footer={<Giscus /> footer}
+        pageFooter={<Giscus />}
+        footer={footer}
       />
     </>
   )
