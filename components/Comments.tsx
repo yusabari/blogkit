414cb0641b4013955d922c40f41b3b9561436ae2
yusabari <152364753+yusabari@users.noreply.github.com>
2024-01-16 13:08:48 +0900

    Update Comments.tsx
    change giscus theme to 'preferred_color_scheme' from dark

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -27,7 +27,7 @@ export default function Giscus() {
     scriptElem.setAttribute('data-reactions-enabled', '1');
     scriptElem.setAttribute('data-emit-metadata', '0');
     scriptElem.setAttribute('data-input-position', 'bottom');
-    scriptElem.setAttribute('data-theme', 'noborder_dark');
+    scriptElem.setAttribute('data-theme', 'preferred_color_scheme');
     scriptElem.setAttribute('data-lang', 'ko');
 
     ref.current.appendChild(scriptElem);
